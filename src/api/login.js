a602d91dc90373214622d656c4573a44f09ff585
root <root@localhost.localdomain>
2019-01-15 07:34:57 -0500

    初始化

---
+++ src/api/login.js
@@ -0,0 +1,46 @@
+import request from '@/utils/request'
+
+export function loginByUsername(username, password) {
+  const data = {
+    username,
+    password
+  }
+  return request({
+    url: '/login/login',
+    method: 'post',
+    data
+  })
+}
+export function loginByAlipay() {
+  return request({
+    url: '/api/v0/login/authUrl',
+    method: 'get'
+  })
+}
+export function loginByThirdparty(status, email, code) {
+  const data = {
+    status,
+    email,
+    code
+  }
+  console.log(data)
+  return request({
+    url: '/api/v0/login/authRedirect',
+    method: 'post'
+  })
+}
+export function logout() {
+  return request({
+    url: '/login/logout',
+    method: 'post'
+  })
+}
+
+export function getUserInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
