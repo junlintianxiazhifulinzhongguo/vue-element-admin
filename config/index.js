5395e80b9aa67c2cbad6812a19803e0f5d48290c
root <root@localhost.localdomain>
2019-01-15 07:39:01 -0500

    修改端口

--- config/index.js
+++ config/index.js
@@ -15,8 +15,8 @@ module.exports = {
 
     // can be overwritten by process.env.HOST
     // if you want dev by ip, please set host: '0.0.0.0'
-    host: '192.168.1.19',
-    port: 9527, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: 'localhost',
+    port: 3015, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: false,
