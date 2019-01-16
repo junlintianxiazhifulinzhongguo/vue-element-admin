21805aee35ba1e2678bf2176da9a039907d8cf7d
root <root@localhost.localdomain>
2019-01-16 02:26:21 -0500

    ddk

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://www.junlintianxiazhifulinzhongguo.top/%23/"'
+  BASE_API: '"http://www.junlintianxiazhifulinzhongguo.top/"'
 }
