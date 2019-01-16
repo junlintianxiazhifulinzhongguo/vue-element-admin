0fa345db0c7d55e9b481d11dd8e39c4bafc9719d
root <root@localhost.localdomain>
2019-01-16 05:02:17 -0500

    assjkddkdddjhddd

--- src/api/login.js
+++ src/api/login.js
@@ -25,7 +25,7 @@ export function loginByThirdparty(status, email, code) {
   }
   console.log(data)
   return request({
-    url: 'api/v0/login/authRedirect',
+    url: 'login/authRedirect',
     method: 'post'
   })
 }
