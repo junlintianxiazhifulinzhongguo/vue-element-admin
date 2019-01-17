0de2083926e7557200249b604bf50ba4931e35df
root <root@localhost.localdomain>
2019-01-17 04:49:28 -0500

    adf

--- src/views/login/authredirect.vue
+++ src/views/login/authredirect.vue
@@ -2,10 +2,18 @@
 export default {
   name: 'AuthRedirect',
   created() {
-    const hash = window.location.search.slice(1)
+    console.log(111)
+    const hash = window.location.hash.slice(1)
     console.log(hash)
     window.opener.location.href = window.location.origin + '/login#' + hash
     window.close()
   }
+  // mounted() {
+  //   console.log(222)
+  //   const hash = window.location.search.slice(1)
+  //   console.log(hash)
+  //   window.opener.location.href = window.location.origin + '/login#' + hash
+  //   window.close()
+  // }
 }
 </script>
