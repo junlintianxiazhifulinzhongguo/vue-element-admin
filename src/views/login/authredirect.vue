f35d1b4618f458cebfb52f4df6a0abd43a0243c7
root <root@localhost.localdomain>
2019-01-29 05:37:15 -0500

    safdasfsdf

--- src/views/login/authredirect.vue
+++ src/views/login/authredirect.vue
@@ -1,10 +1,24 @@
+<template>
+  <div class="authredirect">
+    <img src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif" alt="" srcset="">
+    {{passwordType}}
+  </div>
+</template>
+
 <script>
 export default {
   name: 'AuthRedirect',
+  data(){
+    return {
+      hash: null,    
+    }
+  },
   created() {
     console.log(111)
     const hash = window.location.hash.slice(1)
     console.log(hash)
+    this.hash = hash
+    alert(hash)
     window.opener.location.href = window.location.origin + '/#' + hash
     window.close()
   }
