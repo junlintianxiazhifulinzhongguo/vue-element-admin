71da41537cf87752fee55ed758e83c0db3660d96
root <root@localhost.localdomain>
2019-02-07 05:10:25 -0500

    2212

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://junlintianxiazhifulinzhongguo.top/api/v0/"'
+  BASE_API: '"http://www.junlintianxiazhifulinzhongguo.top/api/v0/"'
 }
